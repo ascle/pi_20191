6d6301799c18cab9e542ed819fe4ea6f5c0aa1bc
ascle <user@host.host>
2019-05-28 14:43:09 -0300

    Função main, comentarios

--- pi_ascle.py
+++ pi_ascle.py
@@ -7,7 +7,7 @@ from PIL import Image
 IMG_LENNA_JPG = '/home/aluno/Imagens/lena.jpeg'
 IMG_LENNA_PNG = '/home/aluno/Imagens/lena.png'
 
-# usar sempre o dtype int8
+# usar sempre o dtype int8 
 def abrir(path):
     return np.asarray(Image.open(path))
 
@@ -16,6 +16,12 @@ def mostrar(img):
     plt.imshow(img)
     plt.show()
 
+# se for uma imagem grayscale
 def ncanais(img)    :
     return img.shape[2]
 
+
+if __name__ == "__main__":
+	lena_jpg = abrir(IMG_LENNA_JPG)
+	lena_png = abrir(IMG_LENNA_PNG)
+ 
