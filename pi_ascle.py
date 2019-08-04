893dfd508599901d0fb1f3e6a30314efcc7c4309
ascle <user@host.host>
2019-08-04 17:47:34 -0300

    4 concluido e 5 em contrução

--- pi_ascle.py
+++ pi_ascle.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import matplotlib.cm as cm
@@ -6,9 +8,10 @@ import numpy as np
 
 from PIL import Image
 
-IMG_LENNA_JPG = '/home/aluno/Imagens/lena.jpeg'
-IMG_LENNA_PNG = '/home/aluno/Imagens/lena.png'
-IMG_LENNA_GS = '/home/aluno/Imagens/lenna_grey.jpg'
+IMG_LENNA_JPG = 'imagens/lena.jpeg'
+IMG_LENNA_50 =  'imagens/lena50.jpeg'
+IMG_LENNA_PNG = 'imagens/lena.png'
+IMG_LENNA_GS =  'imagens/lenna_grey.jpg'
 
 
 
@@ -43,15 +46,30 @@ def ncanais(img):
 			return 1
 	else:
 		raise TypeError("Image must be a numpy.ndarray")
-		
-		
-		
+
+
+# 4 - Crie uma função chamada size que retorna um vetor onde a primeira posição é a largura
+# e a segunda é a altura em pixels da imagem de entrada.
+def size(img):
+	if type(img) is np.ndarray:
+		return np.array([img.shape[0], img.shape[1]])
+	else:
+		raise TypeError("Image must be a numpy.ndarray")
+
+
+
 # 5 - rgb2gray g = 0,299r + 0,587g + 0,114b + 0,0a     entrada imagem  retorna imagem
 def rgb2gray(img):
-    if ncanais(img) == 1:
-		return img
-	
-		
+	if type(img) is np.ndarray:
+		if ncanais(img) == 1:
+			return img
+		else:
+			for r in img:
+				for c in r:
+					(c[0]*0,299) + (c[1]*0,587) + (c[2]*0,114)
+	else:
+		raise TypeError("Image must be a numpy.ndarray")
+
 
 
 
@@ -64,7 +82,14 @@ def rgb2gray(img):
 #mostrar(lena_png)
 
 
-lena_gs = abrir(IMG_LENNA_GS)
-mostrar(lena_gs)
-#lena_jpg = abrir(556)
-mostrar(IMG_LENNA_GS)
+#lena_gs = abrir(IMG_LENNA_50)
+#mostrar(lena_gs)
+#print(type(lena_jpg.shape))
+#print(lena_jpg.shape)
+#print(type(size(lena_jpg)))
+#print(lena_jpg)
+
+lena_jpg = abrir(IMG_LENNA_50)
+print(size(lena_jpg))
+rgb2gray(lena_jpg)
+
